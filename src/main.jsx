cb380d3f273e68b3b094cdd9bd3544e7b91edf48
Sviatoslav <sviat.danko@gmail.com>
2025-03-13 21:35:54 +0200

    move app

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 import 'modern-normalize';
 import './index.css';
 import './reset.css';
-import App from './App.jsx';
+import App from './components/App.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
