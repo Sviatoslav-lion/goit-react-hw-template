460db872d3c52ef47332f4c1de218558f6d09b30
Sviatoslav <sviat.danko@gmail.com>
2025-03-19 06:40:32 +0000

    Add base style

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import 'modern-normalize';
 import './index.css';
 import './reset.css';
+import './base.css';
 import App from './components/App.jsx';
 
 createRoot(document.getElementById('root')).render(
